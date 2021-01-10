95f1797b3cd4b566a0def36dcf47d9ec5c8366a8
Martin Schrodt <martin@schrodt.org>
2021-01-10 01:49:50 +0100

    add StateDifferenceEvaluator to main clarity project

--- src/main/java/skadistats/clarity/analyzer/replay/ObservableEntity.java
+++ src/main/java/skadistats/clarity/analyzer/replay/ObservableEntity.java
@@ -13,12 +13,12 @@ import skadistats.clarity.io.s2.S2DTClass;
 import skadistats.clarity.model.DTClass;
 import skadistats.clarity.model.FieldPath;
 import skadistats.clarity.model.state.EntityState;
+import skadistats.clarity.util.StateDifferenceEvaluator;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Objects;
 
 @Slf4j
 public class ObservableEntity extends ObservableListBase<ObservableEntityProperty> {
@@ -93,94 +93,35 @@ public class ObservableEntity extends ObservableListBase<ObservableEntityPropert
     }
 
     public void performCountChanged(EntityState state) {
+        EntityState oldState = this.state;
         this.state = state;
+
         beginChange();
-        new CountUpdater(state.fieldPathIterator()).updateCount();
+        new StateDifferenceEvaluator(oldState, state) {
+            @Override
+            protected void onPropertiesDeleted(List<FieldPath> fieldPaths) {
+                int idx = Collections.binarySearch(properties, fieldPaths.get(0));
+                List<ObservableEntityProperty> subList = properties.subList(idx, idx + fieldPaths.size());
+                List<ObservableEntityProperty> removed = new ArrayList<>(subList);
+                subList.clear();
+                nextRemove(idx, removed);
+            }
+            @Override
+            protected void onPropertiesAdded(List<FieldPath> fieldPaths) {
+                int idx = -Collections.binarySearch(properties, fieldPaths.get(0)) - 1;
+                int size = fieldPaths.size();
+                List<ObservableEntityProperty> added = new ArrayList<>(size);
+                for (FieldPath fieldPath : fieldPaths) {
+                    added.add(createProperty(fieldPath));
+                }
+                properties.addAll(idx, added);
+                nextAdd(idx, idx + size);
+            }
+            @Override
+            protected void onPropertyChanged(FieldPath fieldPath) {
+            }
+        }.work();
         endChange();
-    }
-
-    private class CountUpdater {
-
-        private Iterator<FieldPath> leftIter;
-        private int rightIdx = -1;
-        private FieldPath left = null;
-        private FieldPath right = null;
-        private List<ObservableEntityProperty> akku = new ArrayList<>();
-
-        public CountUpdater(Iterator<FieldPath> leftIter) {
-            this.leftIter = leftIter;
-        }
-
-        private void advanceLeft() {
-            left = leftIter.hasNext() ? leftIter.next() : null;
-        }
-
-        private void advanceRight() {
-            if (rightIdx <= properties.size()) {
-                rightIdx++;
-                if (rightIdx < properties.size()) {
-                    right = properties.get(rightIdx).getFieldPath();
-                } else {
-                    right = null;
-                }
-            } else {
-                throw new UnsupportedOperationException("should never happen");
-            }
-        }
-
-        private boolean rightHigher() {
-            return left != null && (right == null || right.compareTo(left) > 0);
-        }
-
-        private boolean leftHigher() {
-            return right != null && (left == null || left.compareTo(right) > 0);
-        }
-
-        private void invalidateAkkuPropertyBindings() {
-            if (propertyBindings == null) return;
-            for (ObservableEntityProperty p : akku) {
-                int idx = Collections.binarySearch(propertyBindings, p.getFieldPath());
-                if (idx < 0) continue;
-                propertyBindings.get(idx).invalidate();
-            }
-        }
-
-        private void updateCount() {
-            advanceLeft();
-            advanceRight();
-            while (true) {
-                if (Objects.equals(left, right)) {
-                    if (left == null) break;
-                    advanceLeft();
-                    advanceRight();
-                } else if (rightHigher()) {
-                    do {
-                        akku.add(createProperty(left));
-                        advanceLeft();
-                    } while (rightHigher());
-                    int n = akku.size();
-                    properties.addAll(rightIdx, akku);
-                    nextAdd(rightIdx, rightIdx + n);
-                    rightIdx += n;
-                    invalidateAkkuPropertyBindings();
-                    akku.clear();
-                } else if (leftHigher()) {
-                    int baseIdx = rightIdx;
-                    do {
-                        akku.add(properties.get(rightIdx));
-                        advanceRight();
-                    } while (leftHigher());
-                    int n = akku.size();
-                    properties.removeAll(akku);
-                    nextRemove(baseIdx, akku);
-                    rightIdx -= n;
-                    invalidateAkkuPropertyBindings();
-                    akku.clear();
-                } else {
-                    throw new UnsupportedOperationException("should never happen");
-                }
-            }
-        }
     }
 
     public DTClass getDtClass() {
